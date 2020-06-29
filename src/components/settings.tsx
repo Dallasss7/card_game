cdb9528f2f879bccf96f11c0dbaef0eda16a2f59
Dallasss7 <mr.dalsummers@gmail.com>
2020-06-28 17:46:39 -0700

    added confirm modal to settings

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -4,7 +4,9 @@ import {
     StyleSheet,
     Text,
     SectionList,
-    TouchableOpacity
+    TouchableOpacity,
+    Modal,
+    TouchableHighlight
 } from 'react-native';
 import IconAntDesign from 'react-native-vector-icons/AntDesign';
 import PropTypes from 'prop-types';
@@ -12,22 +14,26 @@ import PropTypes from 'prop-types';
 import { globalObj } from '../global';
 
 export default function Settings(): JSX.Element {
-    const [isSelected, setSelected] = useState(false);
-    const selectDeck = (selection) => {
-        globalObj.deckType = selection;
-        return setSelected((previousState) => !previousState);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedValue, setSelectedValue] = useState(0);
+
+    const selectDeck = () => {
+        globalObj.deckType = selectedValue;
     };
+
     const Decks = [
         {
             title: 'Deck Types',
             data: ['Letters', 'Numbers']
         }
     ];
+
     const DeckItems = ({ title, selection }) => (
         <TouchableOpacity
             style={styles.item}
             onPress={() => {
-                selectDeck(selection);
+                setSelectedValue(selection);
+                setModalVisible(true);
             }}
         >
             <Text style={styles.title}>
@@ -52,30 +58,68 @@ export default function Settings(): JSX.Element {
     // };
 
     return (
-        <View style={styles.container}>
-            {/* ** DISABLING DIFFICULTY SETTINGS **
-	<View style={styles.difficulty}>
-		<Switch
-			trackColor={{ false: "#767577", true: "#81b0ff" }}
-			thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-			ios_backgroundColor="#3e3e3e"
-			onValueChange={toggleSwitch}
-			value={isEnabled}
-		/>
-		<Text style={styles.switch_text}>Increase Difficulty?</Text>
-	</View> */}
-            <View style={styles.decks}>
-                <View>
-                    <SectionList
-                        sections={Decks}
-                        keyExtractor={(item, index) => item + index}
-                        renderItem={({ item, index }) => (
-                            <DeckItems title={item} selection={index} />
-                        )}
-                        renderSectionHeader={({ section: { title } }) => (
-                            <Text style={styles.header}>{title}</Text>
-                        )}
-                    />
+        <View style={styles.centeredView}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Save Changes?</Text>
+                        <View style={styles.modal_buttons}>
+                            <TouchableHighlight
+                                style={{
+                                    ...styles.openButton,
+                                    backgroundColor: '#2196F3'
+                                }}
+                                onPress={() => {
+                                    selectDeck();
+                                    setModalVisible(!modalVisible);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>Yes</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight
+                                style={{
+                                    ...styles.openButton,
+                                    backgroundColor: '#2196F3'
+                                }}
+                                onPress={() => {
+                                    setModalVisible(!modalVisible);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>No</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+            <View style={styles.container}>
+                {/* ** DISABLING DIFFICULTY SETTINGS **
+				<View style={styles.difficulty}>
+					<Switch
+						trackColor={{ false: "#767577", true: "#81b0ff" }}
+						thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+						ios_backgroundColor="#3e3e3e"
+						onValueChange={toggleSwitch}
+						value={isEnabled}
+					/>
+					<Text style={styles.switch_text}>Increase Difficulty?</Text>
+				</View> */}
+                <View style={styles.decks}>
+                    <View>
+                        <SectionList
+                            sections={Decks}
+                            keyExtractor={(item, index) => item + index}
+                            renderItem={({ item, index }) => (
+                                <DeckItems title={item} selection={index} />
+                            )}
+                            renderSectionHeader={({ section: { title } }) => (
+                                <Text style={styles.header}>{title}</Text>
+                            )}
+                        />
+                    </View>
                 </View>
             </View>
         </View>
@@ -135,5 +179,45 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 16
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    openButton: {
+        backgroundColor: '#F194FF',
+        borderRadius: 10,
+        padding: 10,
+        elevation: 2,
+        margin: 7
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center'
+    },
+    modal_buttons: {
+        flexDirection: 'row'
     }
 });
