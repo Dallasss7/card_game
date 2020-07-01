9b8e471b62199fb54e11dd65cf8d823db99c001b
Dallasss7 <mr.dalsummers@gmail.com>
2020-06-30 21:53:00 -0700

    ignore web and node

--- tests/App.test.js
+++ tests/App.test.js
@@ -2,11 +2,15 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import App from '../App.tsx';
+import { Platform } from 'react-native';
 
 describe('<App />', () => {
     it('has 2 child', () => {
-        const tree = renderer.create(<App />).toJSON();
-        console.log(tree);
-        expect(tree.children.length).toBe(2);
+        if (Platform.OS === 'android' || Platform.OS === 'ios') {
+            const tree = renderer.create(<App />).toJSON();
+            expect(tree.children.length).toBe(2);
+        } else {
+            return;
+        }
     });
 });
